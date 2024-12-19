8a529ad16cbff7bf803d25c391cbe2887575d909
Vachek-hooves <vachek0602@gmail.com>
2024-12-19 12:07:47 +0200

    update config MainActivity.kt

--- android/app/src/main/java/com/admiralislandsbattle/MainActivity.kt
+++ android/app/src/main/java/com/admiralislandsbattle/MainActivity.kt
@@ -4,18 +4,9 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import android.content.Intent;
-import android.content.res.Configuration;
+
 
 class MainActivity : ReactActivity() {
-
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-      super.onConfigurationChanged(newConfig);
-      Intent intent = new Intent("onConfigurationChanged");
-      intent.putExtra("newConfig", newConfig);
-      this.sendBroadcast(intent);
-  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
